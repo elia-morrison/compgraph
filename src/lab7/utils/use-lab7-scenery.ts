90e40f79a16cc551d58d06a8d13cdce2fed761eb
heyra <zalomnova.eliz@gmail.com>
2024-05-04 16:48:51 +0300

    feat: add lantern

--- src/lab7/utils/use-lab7-scenery.ts
+++ src/lab7/utils/use-lab7-scenery.ts
@@ -4,6 +4,8 @@ import { useOrangeModel } from "src/lab7/resources/orange";
 import { UncannyRotatingMovement } from "src/shared/base/movable/movement-types/uncanny-rotating-movement";
 import { Body3D } from "src/shared/base/body-3d";
 import { Euler } from "three";
+import { PointLight } from "src/shared/scenery/light/lightsource";
+import { vec3 } from "gl-matrix";
 
 export const useLab7Scenery = (scene: BaseScene, gl: WebGL2RenderingContext) => {
     const dorimeMesh = useDorimeRatModel();
@@ -19,11 +21,16 @@ export const useLab7Scenery = (scene: BaseScene, gl: WebGL2RenderingContext) => 
     hugeDorime.setRotation(new Euler(0,-Math.PI / 3,0));
     uncannyRotate.attachToMovable(hugeDorime);
 
-    const secondHugeDorime = new Body3D(dorimeMesh);
-    secondHugeDorime.setScale([1, 1, 1]);
-    secondHugeDorime.setPosition([-7, 0, 10]);
-    secondHugeDorime.setRotation(new Euler(0,Math.PI / 3,0));
-    uncannyRotate.attachToMovable(secondHugeDorime);
+    const lanternBody = new Body3D(dorimeMesh);
+    lanternBody.setScale([0.1, 0.1, 0.1]);
+    lanternBody.setPosition([-7, 7, 10]);
+    lanternBody.setRotation(new Euler(0,-2 * Math.PI / 3,0));
+
+    const nimbusLight = new PointLight();
+    const nimbusLightPos = vec3.clone(lanternBody.position);
+    nimbusLight.setPosition(nimbusLightPos);
+    nimbusLight.radius = 10;
+    scene.lightsources.push(nimbusLight);
 
     const hugeOrange = new Body3D(orangeMesh);
     hugeOrange.setScale([0.05, 0.05, 0.05]);
@@ -31,7 +38,7 @@ export const useLab7Scenery = (scene: BaseScene, gl: WebGL2RenderingContext) => 
     hugeOrange.setRotation(new Euler(0,-Math.PI / 3,0));
     uncannyRotate.attachToMovable(hugeOrange);
 
-    scene.objects.push(hugeDorime, secondHugeDorime, hugeOrange);
+    scene.objects.push(hugeDorime, lanternBody, hugeOrange);
 
     return {
         dorimeMesh, orangeMesh
