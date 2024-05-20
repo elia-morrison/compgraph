0dfb4944a082d7de0b8acdd7de89e3311b5382fb
Elia Morrison <andsimof@gmail.com>
2024-05-20 11:41:01 +0300

    add particle system selection CLI

--- src/lab8/lab8.ts
+++ src/lab8/lab8.ts
@@ -17,23 +17,33 @@ camera.setPosition([0, 0, -3]);
 
 let glManager = new GLManager(webgl_context, camera);
 
-let fire = new FireworksParticleSystem(webgl_context, function () {
-    glManager.init(fire);
+function initParticleSystem(ParticleClass: { new(gl: WebGL2RenderingContext, onload: CallableFunction): ParticleSystem }) {
+    let pS = new ParticleClass(webgl_context, function () {
+        glManager.init(pS);
 
-    let update = function (ts: number) {
-        fire.setRotation(new Euler(0, 2 * glManager.totalTime / 1000. * Math.PI - Math.PI / 2, 0));
-        glManager.render(ts);
+        let update = function (ts: number) {
+            pS.setRotation(new Euler(0, 2 * glManager.totalTime / 1000. * Math.PI - Math.PI / 2, 0));
+            glManager.render(ts);
+            window.requestAnimationFrame(update);
+        };
+
         window.requestAnimationFrame(update);
-    };
+    });
+}
 
-    window.requestAnimationFrame(update);
-});
+initParticleSystem(FireParticleSystem);
 
 $('#term').terminal({
     sparkler: function () {
 
     },
+    fireworks: function () {
+        initParticleSystem(FireworksParticleSystem);
+    },
+    fire: function () {
+        initParticleSystem(FireParticleSystem);
+    }
 }, {
     greetings: 'WebGL / Lab8',
-    completion: ['sparkler'],
+    completion: ['sparkler', 'fireworks'],
 });
